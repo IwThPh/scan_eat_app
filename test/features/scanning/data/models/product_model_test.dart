5e68f81ad62ea7ccf1404d9b84f1cc4ef673dc63
Iwan Phillips <iwanthomphil@gmail.com>
2019-11-11 12:48:08 +0000

    Changed to use sample data.

--- test/features/scanning/data/models/product_model_test.dart
+++ test/features/scanning/data/models/product_model_test.dart
@@ -1,23 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:food_label_app/features/scanning/domain/entities/product.dart';
 
-import '../../../../../lib/features/scanning/data/models/product_model.dart';
-import '../../../../../lib/features/scanning/domain/entities/product.dart';
+import '../../../../samples.dart';
 
 void main() {
-  final tProductModel = ProductModel(
-    barcode: "0000000000000",
-    name: "Test",
-    carbohydrate_100g: 0.0,
-    energy_100g: 0.0,
-    fat_100g: 0.0,
-    fiber_100g: 0.0,
-    protein_100g: 0.0,
-    salt_100g: 0.0,
-    saturates_100g: 0.0,
-    sodium_100g: 0.0,
-    sugars_100g: 0.0,
-    weight_g: 0.0
-  );
+  final tProductModel = Samples.tProductModel;
 
   test('Should be a subclass of Product entity', () async {
     expect(tProductModel, isA<Product>());
