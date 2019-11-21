8a12c06f1c8b3dfd6de91248e9faa096700049a6
Iwan Phillips <iwanthomphil@gmail.com>
2019-11-21 14:57:47 +0000

    Modified to match Mockup.

--- lib/assets/theme/app_theme.dart
+++ lib/assets/theme/app_theme.dart
@@ -14,7 +14,7 @@ class AppTheme {
       display1: TextStyle(fontSize: 34.0, fontWeight: FontWeight.w400),
       headline: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w400),
       title: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
-      subhead: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+      subhead: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w700),
       body2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
       body1: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400),
       caption: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400),
