3568f1fa95c846dc7e8526935c94dd5422600590
Iwan Phillips <iwanthomphil@gmail.com>
2019-12-30 17:16:09 +0000

    Added custom loading animation.

--- lib/features/scanning/presentation/widgets/loading_widget.dart
+++ lib/features/scanning/presentation/widgets/loading_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
@@ -8,8 +9,16 @@ class LoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 256,
+      height:256,
       child: Center(
-        child: CircularProgressIndicator(),
+        child: FlareActor(
+
+          "lib/assets/animations/Loading.flr",
+          alignment: Alignment.center,
+          fit: BoxFit.contain,
+          animation: "go",
+        ),
       ),
     );
   }
