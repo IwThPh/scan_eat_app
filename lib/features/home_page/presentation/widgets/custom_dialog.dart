32465746748c0af7a4c36d9c6b4e22c151e5064f
Iwan Phillips <iwanthomphil@gmail.com>
2020-02-26 18:42:30 +0000

    Added Custom Dialog.

---
+++ lib/features/home_page/presentation/widgets/custom_dialog.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:scaneat/assets/theme/colours.dart';
+
+class CustomDialog extends StatelessWidget {
+  final String title;
+  final Widget content;
+
+  CustomDialog({
+    @required this.title,
+    @required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  dialogContent(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            top: 20 + 16.0,
+            bottom: 16.0,
+            left: 16.0,
+            right: 16.0,
+          ),
+          margin: EdgeInsets.only(top: 16.0),
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                offset: const Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // To make the card compact
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              content,
+              SizedBox(height: 24.0),
+            ],
+          ),
+        ),
+        Positioned(
+          left: 16.0,
+          right: 16.0,
+          child: CircleAvatar(
+            child: Icon(
+              Icons.edit,
+              color: Colors.white,
+            ),
+            backgroundColor: Colours.green,
+            radius: 25,
+          ),
+        ),
+      ],
+    );
+  }
+}
